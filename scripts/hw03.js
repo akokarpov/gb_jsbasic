d24f44df771c65ac447176907756c6b7eb06df2f
akokarpov <akokarpov@gmail.com>
2021-04-11 13:27:11 +0300

    minor changes

--- scripts/hw03.js
+++ scripts/hw03.js
@@ -1,11 +1,16 @@
 
 // 1.	С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
 
-let n = 0;
-while (n < 101) {
-    console.log(n);
-    n += 1;
+var userNumber = 100;
+var list = [];
+notPrime:
+for (var i = 2; i <= userNumber; i++) {
+    for (var j = 2; j < i; j++) {
+        if (i % j == 0) continue notPrime;
+    }
+    list.push(i);
 }
+console.log(list);
 
 // 2.	С этого урока начинаем работать с функционалом интернет-магазина. Предположим, есть сущность корзины. Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров. 
 // 3.	Товары в корзине хранятся в массиве. Задачи:
@@ -13,7 +18,7 @@ while (n < 101) {
 // b.	Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
 
 function countBasketPrice(arr) {
-    
+
     let total_price = 0;
 
     for (let item in arr) {
@@ -30,13 +35,13 @@ let basket = {
     "bicycle": 40000,
 }
 
-console.log(countBasketPrice(arr = basket));
+console.log(countBasketPrice(basket));
 
 // 4.	* Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. Выглядеть это должно так:
 // for(...){// здесь пусто}
 //  
 
-for(let i = 0; i < 10; console.log(i++)){}
+for (let i = 0; i < 10; console.log(i++)) { }
 
 // 5.	 * Нарисовать пирамиду с 20 рядами с помощью console.log, как показано на рисунке:
 // x
